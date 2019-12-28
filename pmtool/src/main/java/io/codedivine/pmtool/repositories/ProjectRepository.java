e1efcdecdefc371f1f91e411ac1ca3939618a5cc
nikolavelkovski <nikola.niksi1@outlook.com>
2019-12-28 13:19:43 +0100

    Project entity and Repository

---
+++ pmtool/src/main/java/io/codedivine/pmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.codedivine.pmtool.repositories;
+
+import io.codedivine.pmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
