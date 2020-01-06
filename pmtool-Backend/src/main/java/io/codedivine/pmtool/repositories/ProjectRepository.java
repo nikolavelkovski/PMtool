6247c3ac23de650e205667faf54920333013284a
nikolavelkovski <nikola.niksi1@outlook.com>
2020-01-06 19:24:40 +0100

    Spring BackEnd basic operations

---
+++ pmtool-Backend/src/main/java/io/codedivine/pmtool/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package io.codedivine.pmtool.repositories;
+
+import io.codedivine.pmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    Project findProjectByProjectIdentifier(String projectId);
+
+   @Override
+   Iterable<Project> findAll();
+
+}
